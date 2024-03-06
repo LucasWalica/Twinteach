666d10555493fe1ab97f140d6fc986b8c6b95299
bul86846 <145587827+bul86846@users.noreply.github.com>
2024-03-06 17:16:03 +0100

    revertir paridas

---
+++ tablas_inserts.sql
@@ -0,0 +1,22 @@
+1-crear cuenta-> 
+
+INSERT INTO ALUMNO(CORREO, NOMBRE, CONTRASEÑA) VALUES (
+    ("pepito@correo.com", "PepitoPiscinas", "hunter2")
+)
+
+2-login->
+SELECT ID FROM ALUMNO WHERE CORREO = 'pepito@correo.com' AND CONTRASEÑA = "hunter2";
+
+3-cambiar icono
+
+UPDATE TABLE ALUMNO_CLASE SET ICONO = "urlejemplo2";
+
+4
+
+INSERT INTO PROFESOR (NOMBRE, EMAIL) VALUES (
+    ("fonsi", "fonsifonsifonsi@fonsi.fonsi");
+);
+
+5-login profesor
+
+SELECT ID FROM PROFESOR WHERE NOMBRE="F"
