1f708a6d10a40769d9889be9da6cd0e68d1a5030
LucasWalica <lucasw58@educastur.es>
2024-03-06 17:26:41 +0100

    qw

---
+++ ayuda.sql
@@ -0,0 +1,6 @@
+insert into alumno(correo, nombre, CONTRASEÑA, icono)
+values
+    ("pepitoGrillo@gmail.com", "pepito", "efoiuhwvñonew", "https://www.iconoguapo.com")
+    ("Almendrita21@gmail.com", "Almendrita", "winvewofnweoi", "https://www.iconoMasGuapo.com")
+    ("AlejandroCak@gmail.com", "Aleks", "oifwoivwoi", "https://www.iconoNoTanGuapo.es")
+    ("Alejandra@gmail.com", "Alejandra", "iejfiewjfew", "https://www.iconopocho.com")
