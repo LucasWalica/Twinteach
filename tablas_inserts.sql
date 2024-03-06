e7d6604b2b739437a10f9274b633cb1cbb45936d
LucasWalica <lucasw58@educastur.es>
2024-03-06 16:49:17 +0100

    corregido

---
+++ tablas_inserts.sql
@@ -0,0 +1,17 @@
+INSERT INTO Alumno(correo, nombre, CONTRASEÑA)
+values 
+("PepitoGrillo@gmail.com", "Pepito", "fofigrill")
+("MariaPerez@gmail.com", "Maria", "jklipol")
+("Ferna@gmail.com", "Fernando", "fer45390")
+("Marta@mail.com", "Marta", "klio34")
+
+INSERT INTO PROFESOR(CORREO, NOMBRE, CONTRASEÑA)
+values
+("BERTO@gmail.com", "Roberto", "jsklsfeoe")
+("Maria@gmail.com", "Maria", "jasdojvndoie")
+("Lucrecia@gmail.com", "Lucrecia", "hjcjcjeie")
+
+INSERT INTO aceptar_alumno(ID_ALUMNO, ID_Profesor, alumno_aceptado)
+values
+(1, 1, 1),
+()
